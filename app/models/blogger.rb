ec98df94990be1bc8932fd16a4835cf16d1345c7
Colin <cstatro@gmail.com>
2019-07-24 11:32:28 -0400

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,13 @@
 class Blogger < ApplicationRecord
+    has_many :posts
+    has_many :destinations, through: :posts
+    validates :name, uniqueness: true
+    validates :age, numericality:{greater_than: 0}
+    validate :valid_bio
 
+    def valid_bio
+        if self.bio.split.length < 30 
+            errors.add(:bio, "must be longer than 30 words holmes.")
+        end
+    end
 end
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,8 @@
 class Destination < ApplicationRecord
+has_many :posts
+has_many :bloggers, through: :posts
 
+def uniq_bloggers
+    self.posts.map {|p| p.blogger}.uniq
+end
 end
